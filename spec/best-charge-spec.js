d9d3ede3f2662be73e24033fd7b0a0715af6eb45
Galileo2010 <33446456+Galileo2010@users.noreply.github.com>
2019-02-24 18:08:59 +0800

    通过测试 2019-2-24 18:08:22

--- spec/best-charge-spec.js
+++ spec/best-charge-spec.js
@@ -45,4 +45,55 @@ describe('Take out food', function () {
     expect(summary).toEqual(expected)
   });
 
+  // 字符串拆分测试
+  it('test split',()=>{
+    let item = "ITEM0013 x 4";
+    let result = item.split(" x ");
+    expect(result[0]).toEqual("ITEM0013");
+    expect(result[1]).toEqual("4");
+  });
+  // 生成原始菜单1测试
+  it('test getItemList1', ()=>{
+    let inputs = ["ITEM0013 x 4", "ITEM0022 x 1"];
+    let result = getItemList(inputs);
+    expect(result).toEqual([["ITEM0013",4],["ITEM0022",1]]);
+    inputs = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
+    result = getItemList(inputs);
+    expect(result).toEqual([["ITEM0001",1],["ITEM0013",2],["ITEM0022",1]]);
+  });
+  // 由原始菜单生成详细清单测试
+  it('test getSuperItemList', ()=>{
+    let inputs = ["ITEM0013 x 4"];
+    let result = getItemList(inputs);
+    let allitems = loadAllItems();
+    result = getSuperItemList(result,allitems);
+    expect(result).toEqual([["ITEM0013",4,'肉夹馍',6.00]]);
+  });
+  // 由详细清单计算原价
+  it('test getNoDiscount', ()=>{
+    let superItemList = [["ITEM0013",4,'肉夹馍',6.00],["ITEM0013",2,'肉夹馍',9.00]];
+    let money = getNoDiscountPrice(superItemList);
+    sum = 4 * 6.00 + 2 * 9.00;
+    expect(money).toEqual(sum);
+  });
+  // 计算方案1的折扣
+  it('test getDiscount1', ()=>{
+    let money = getDiscount1(30.1);
+    expect(money).toEqual(6);
+    money = getDiscount1(29.9);
+    expect(money).toEqual(0);
+  });
+ // 计算方案2的折扣
+  it('test getDiscount2', ()=>{
+    let superItemList = [["ITEM0001",4,'肉夹馍',6.00],["ITEM0022",2,'肉夹馍',9.00]];
+    let money = getDiscount2(superItemList,["ITEM0001","ITEM0022"]);
+    sum = 4 * 6.00 * 0.5 + 2 * 9.00 * 0.5;
+    expect(money).toEqual(sum);
+    money = getDiscount2(superItemList,["ITEM0000","ITEM0022"]);
+    sum = 2 * 9.00 * 0.5;
+    expect(money).toEqual(sum);
+    money = getDiscount2(superItemList,["ITEM0000","ITEM0021"]);
+    sum = 0;
+    expect(money).toEqual(sum);
+  });
 });
--- src/best-charge.js
+++ src/best-charge.js
@@ -1,3 +1,89 @@
 function bestCharge(selectedItems) {
-  return /*TODO*/;
+  let splitters = ['============= 订餐明细 =============\n','-----------------------------------\n','==================================='];
+  // 打印表头
+  let summary = splitters[0];
+  // 打印清单
+  let allItems = loadAllItems();
+  let promotions = loadPromotions();
+  let superSelectedItems = getSuperItemList(getItemList(selectedItems),allItems);
+  for(let i = 0, len = superSelectedItems.length; i < len; i++)
+    summary += superSelectedItems[i][2]+' x '+ superSelectedItems[i][1] + ' = '+ superSelectedItems[i][1]*superSelectedItems[i][3]+'元\n';
+  summary += splitters[1];
+  // 打印折扣信息
+  let sum = getNoDiscountPrice(superSelectedItems);
+  let discount1 = getDiscount1(sum);
+  let discount2 = getDiscount2(superSelectedItems, promotions[1].items);
+  let discount = 0;
+  if(discount2 > discount1)
+  {
+    // 方案2
+    discount = discount2;
+    let discountinfo ='使用优惠:\n' + promotions[1].type;
+    // 打印半价菜品名称
+    discountinfo += '(';
+    let discountItem = promotions[1].items;
+    for(let i = 0, len1 = discountItem.length; i < len1; i++)
+      for(let j = 0, len2 = allItems.length; j < len2; j++)
+      if(discountItem[i] === allItems[j].id)
+      {
+        discountinfo += allItems[j].name;
+        if(i !== len1 - 1) discountinfo += '，';
+        break;
+      }
+    discountinfo += ')'; 
+    summary += discountinfo + '，省'+ discount +'元\n' + splitters[1];
+  }
+  else if(discount1 === 6)
+  {
+    discount = discount1;
+    let discountinfo ='使用优惠:\n' + promotions[0].type;
+    summary += discountinfo + '，省'+ discount +'元\n' + splitters[1];
+  }
+  // 打印总计付款
+  summary += '总计：'+ (sum - discount) +'元\n' + splitters[2];
+  return summary;
+}
+
+function getItemList(inputs){
+  let result = [];
+  for(let i = 0, len = inputs.length; i < len; i++)
+  {
+    let item = inputs[i].split(" x ");
+    result.push([item[0], parseFloat(item[1])]);
+  }
+  return result;
+}
+
+function getSuperItemList(ItemList, allItems){
+  for(let i = 0, len1 = ItemList.length; i < len1; i++)
+    for(let j = 0, len2 = allItems.length; j < len2; j++)
+      if(ItemList[i][0] === allItems[j].id)
+      {
+        ItemList[i]= ItemList[i].concat([allItems[j].name, allItems[j].price]);
+        break;
+      }
+  return ItemList;
+}
+
+function getNoDiscountPrice(superItemList){
+  let sum = 0;
+  for(let i = 0, len = superItemList.length; i < len; i++)
+    sum += superItemList[i][1] * superItemList[i][3];
+  return sum;
+}
+
+function getDiscount1(money){
+  return money >= 30 ? 6 : 0;
+}
+
+function getDiscount2(superItemList, promotItems){
+  let result = 0;
+  for(let i = 0, len1 = superItemList.length; i < len1; i++)
+    for(let j = 0, len2 = promotItems.length; j < len2; j++)
+  if(superItemList[i][0] === promotItems[j])
+  {
+    result += superItemList[i][1] * superItemList[i][3] * 0.5;
+    break;
+  }
+  return result;
 }
